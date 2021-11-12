0bbbc31edfcffac1b70d4d1c4fbfa6ef6836a8eb
Luk4x <luk4xm4ci3l@gmail.com>
2021-11-12 08:15:10 -0300

    a17

---
+++ aula17/aula17.js
@@ -0,0 +1,11 @@
+let people = {
+    name: 'Lucas',
+    sex: 'M',
+    weight: 60.4,
+    gain(w) {
+        this.weight+=w
+    }
+}
+console.log(`${people.name} pesa ${people.weight}`)
+people.gain(2)
+console.log(`${people.name} engordou para ${people.weight}kg`)
